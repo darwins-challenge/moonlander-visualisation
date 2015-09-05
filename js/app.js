3b7c970f23a1b2d42fa793d71b4897b1fb6acec8
dvberkel <daan.v.berkel.1980@gmail.com>
2015-09-05 15:01:19 +0200

    extend model to set crashed property

--- js/app.js
+++ js/app.js
@@ -11,16 +11,21 @@
     var model = {
         "lander": {
             "x": 37, "y": 251,
-            "vx": 0, "vy": 0,
-            "orientation": Math.PI/4, "angular-velocity": 0,
+            "vx": 1, "vy": 0,
+            "orientation": Math.PI/4, "angular-velocity": 0.05,
             "radius": 10,
-            "fuel": 1
+            "fuel": 1,
+            "crashed": false
         },
         "horizon": horizon
     };
 
     var view = new lander.View(model, display);
     function tick(){
+        model.lander.x += model.lander.vx;
+        if (model.lander.x > display.width) {
+            model.lander.x -= display.width;
+        }
         view.update();
         requestAnimationFrame(tick);
     };
